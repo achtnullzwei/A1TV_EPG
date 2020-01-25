69e19af3fc6bc75be2d191dc533c494c2c009f4b
achtnullzwei <mp@achtnullzwei.net>
2020-01-26 00:45:09 +0100

    fix socket path via argparse

--- a1tv_epg.py
+++ a1tv_epg.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Load EPG data from A1 TV official api and convert to XMLTV. Tool is able to load directly into TVHeadend via Socket or output as XML file.')
     parser_group = parser.add_mutually_exclusive_group()
     parser_group.add_argument('-o', metavar='Filename', type=argparse.FileType('wt'), help="Specify valid path and filename. Example: /some/path/filename.xml", dest="o")
-    parser_group.add_argument('-s', metavar='Filename', type=argparse.FileType('wt'), help="Send directly to TVheadend via defined Socket path. Example: /some/path/socketname.sock", dest="s")
+    parser_group.add_argument('-s', metavar='Filename', action="store", help="Send directly to TVheadend via defined Socket path. Example: /some/path/socketname.sock", dest="s")
     parser_group.add_argument('-d', action="store_true", help="Send directly to TVheadend via Socket. Must be set up first in TVheadend first!", dest="d")
     parser.add_argument('-t', metavar='Time in hours', type=int, help="Enter how many hours of epg to load. USE WITH CAUTION! Excessive use may leads to API ban!!! Default: 26, Max: 48", nargs='?', choices=range(1, 49), const=26, default=26, dest="t")
     parser.add_argument('-u', action="store_true", help="Update the channel list JSON file. Calling with -u will not load XMLTV data!", dest="u")
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         elif arguments.o != None:
             save_to_file(data, arguments.o.name)
         elif arguments.s != None:
-            send_to_tvheadend(data, arguments.s.name)
+            send_to_tvheadend(data, arguments.s)
         else:
             save_to_file(data, 'xmltv.xml')
 
